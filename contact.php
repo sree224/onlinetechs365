36a08e5e95ab8069bba1e1e40830b66083564406
Sparue <admin@sparue.com>
2021-08-14 19:02:36 +0530

    first

---
+++ contact.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>Online techs 365</title>
+    
+        <link href="./assets/img/logo.png" rel="icon">
+    
+        <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+            rel="stylesheet">
+    
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+            <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+
+        <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    
+        <link href="assets/css/style1.css" rel="stylesheet">
+    </head>
+    
+<header id="header" style="background:linear-gradient(to right,#4a4a4bba, #1e1e1fcc),  url(./assets/img/contactbg1.jpg)">
+    <div class="overlay"></div>
+    <div class="container d-flex align-items-center">
+        <a href="index.html" class="logo mr-auto"><img src="./assets/img/logonbg.png"></a>
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+                <li ><a href="index.html">Home</a></li>
+                        <li ><a href="about.html">About Us</a></li>
+                        <li><a href="service.html">Services</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li class="active"><a href="contact.php">Contact Us</a></li>
+
+                        <li class="ml-5 text-right"><a href="contact.php"> <i class="bx bx-phone"></i> <b>US:</b> +1-888-5131305 <br> +1-877-5890925</a>
+                            <a href="contact.php"> <i class="bx bx-phone"></i> <b>Uk:</b> +44 800-102-6324</a></li>
+                      
+            </ul>
+        </nav>
+    </div>
+ 
+
+    <div class="container mt-4" data-aos="fade-up">
+        <div class="row text-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="banner-text-wrap ">
+                    <h1>Contact us</h1>  
+                </div>
+                <div class="breadcrumb-top breadcrumb-contact">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">Contact Us</li>
+                        </ol>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6  desclaimer-text  ">
+                <p>
+                    <b>*Disclaimer :</b>  <strong>Online Techs 365 </strong>
+                    provides services for diagnostic and technical support issues for desktops, laptops, mobile devices, and tablets. We are a THIRD PARTY solution and we are NOT AFFILIATED with any brands, trademarks, logos, or any other company names used. The brand names, trademarks, and logos belong to their respective owners and are for representation purposes only.      
+                </p>
+            </div>
+         </div>
+    </div>
+  
+</header>
+
+<section id="about-us" class="about-us section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>We are here to</h2>
+        </div>
+        <div class="row content text-center">
+            <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-up">
+                <h3>Feel free to contact us</h3>
+                <p class="text-justify">
+                    At Online Techs 365, our skilled in house technicians are committed to making sure you get the service
+                    you need, the way you need it. Fill out the form below and we'll contact you within 30 minutes (during regular business hours). Or give us a call, we're always happy to help!</p>
+ 
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section id="about-wrap-top" class="about-wrap-top contact-us-page">
+    <div class="container" data-aos="zoom-in">
+        <div class="row content ">
+         
+            <div class="col-lg-5 md-6 text-left address-wrap p-5">
+                <h3><b><strong>Online Techs 365 </strong>Headquarters:</b></h3>
+                <p class="pt-2"> 
+                    <p><b>Address:</b>  71-75 Shelton Street  Covent Garden, 
+                                        London, WC2H 9JQ </p>
+                    <p> <i class="bx bx-phone"></i> <b>US:</b>  +1-888-5131305 <br> 
+                        <span class="ml-5">+1-877-5890925</span>  </p>
+                       <p> <i class="bx bx-phone"></i> <b>Uk:</b> +44 800-102-6324 </p>
+                    <p><b>Email:</b>support@onlinetechs365.com</p> 
+                    <p><b> Hours:</b>Mon-Friday 7am-9pm PST, Saturday 9am-3pm PST</p> 
+                </p>  
+
+            </div>
+            <div class="col-lg-7 md-8 text-justify">
+                <div class="map-data">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3330.58203436002!2d-111.939487!3d33.408067!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b08c6306aaaab%3A0xae6f13d0b5d235ff!2s64%20E%20Broadway%20Rd%20%23200%2C%20Tempe%2C%20AZ%2085282!5e0!3m2!1sen!2sus!4v1628486604410!5m2!1sen!2sus" width="100%" height="350px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+        </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact" class="contact section-bg clients">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Follow <strong>Online Techs 365 </strong>Online!</h2>
+        </div>
+        <div class="row content text-center">
+            <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-up">
+                <h5>Stay up to date with news about the latest in technology, promos, job listings, and more.
+
+                </h5>
+                    <div class="contact-form pt-5">
+                        <form form action="" method="post" >
+
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+      
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="first_name" placeholder="First name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="last_name" placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+      
+                                <div class="col-sm-4 col-xs-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="phno" placeholder="Contact No">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 col-xs-4"> 
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2 col-xs-2"> </div>
+                                <div class="col-sm-8">
+                                    <div class="form-group">
+                                        <textarea class="form-control" name="message" rows="3"
+                                            placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-xs-2"> </div>
+                            </div>
+                            <div class="btn btn-wrap-form  mt-3">
+                            <input type="submit" name="submit"  class="btn helpbtn">
+                                <!--<button type="button" name="submit" class="btn helpbtn">Submit</button>-->
+                            </div>
+                        </form>
+                    </div>
+               
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<section id="about-us" class="about-us ">
+    <div class="container" data-aos="fade-up">
+        <div class="row content text-center">
+            <div class="col-lg-12  pt-lg-0 " data-aos="fade-up">
+                <p >
+                  <strong>*Disclaimer:</strong>  <strong>Online Techs 365 </strong>provides services for diagnostic and technical support issues for desktops, laptops, mobile devices, and tablets. We are a THIRD PARTY solution and we are NOT AFFILIATED with any brands, trademarks, logos, or any other company names used. The brand names, trademarks, and logos belong to their respective owners and are for representation purposes only.      
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer id="footer" class="footer pt-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-3 col-md-3 text-left">
+                <div class="footer-heading ">
+                    <h5>Contact Online Techs 365</h5>
+                </div>
+                <div class="footer-text mt-4  ">
+                    <p>  
+                        71-75 Shelton Street <br>
+                         Covent Garden, <br>
+                         London, WC2H 9JQ <br> <br>
+                       <p><i class="bx bx-phone"></i> <b>US:</b>  +1-888-5131305 <br> <span class="ml-5">+1-877-5890925</span> </p> 
+                        <i class="bx bx-phone"></i> <b>Uk:</b> +44 800-102-6324 
+
+                    </p>             
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 ">
+                <div class="footer-heading text-center ">
+                    <h6>Quick Links </h6>
+                </div> 
+                <div class="q-links mt-4 text-left">
+                    <ul >
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="service.html">Services</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                   
+            </div>
+
+            <div class="col-lg-3 col-md-3 text-justify">
+                <div class="footer-heading text-left ">
+                    <h6>*Disclaimer: </h6>
+                </div>
+                <p >
+                 <strong>Online Techs 365 </strong>provides services for diagnostic and technical support issues for desktops, laptops, mobile devices, and tablets. We are a THIRD PARTY solution and we are NOT AFFILIATED with any brands, trademarks, logos, or any other company names used. The brand names, trademarks, and logos belong to their respective owners and are for representation purposes only.      
+                </p>
+            </div>
+            
+            <div class="col-lg-3 col-md-3">
+                <div class="footer-heading">
+                    <h6>Other Links </h6>
+                </div>
+
+                <div class="q-links mt-3 text-left">
+                    <ul >
+                        <li><a href="privacy.html">Privacy Policy</a></li>
+                        <li><a href="terms.html">Terms and Conditions</a></li>
+                        <li><a href="sitemap.html">Sitemap</a></li>
+                        <li><a href="terms.html#refund-policy">Refund Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-heading mt-5">
+                    <h6>Social Links </h6>
+                </div> 
+                <div class="social-links  mt-4 ml-5 pl-4  social-icons">
+                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-1">
+                <button onclick="topFunction()" class="back-to-top" id="myBtn" title="Go to top">Top</button>
+
+            </div>
+
+    </div>
+<hr>
+    <div class="row copyright">
+        <div class="col-lg-12 col-md-12 text-center pb-2">
+            <div class="copyright-info">
+                &copy; Copyright   <a href="https://vrmpitta.com/">VRM Pitta</a>. All Rights Reserved.
+    
+            </div>
+        </div>
+    </div>
+  </footer>
+ 
+ <?php 
+if(isset($_POST['submit'])){
+    $to = "mounika.ch@ussdesigners.in"; // this is your Email address
+    $from = $_POST['mounika.ch@ussdesigners.in']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $phno = $_POST['phno'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    
+    
+
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . "Contact No:". $phno . "\n\n" . "Email:" . $email . "\n\n"  . "Message:" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+ 
+  
+<body>
+
+
+
+
+    
+    
+    
+    
+
+
+
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
